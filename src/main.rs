72d171b995599f6aa80cd867ab707ae3f1ca74cc
liboxin <610968110@qq.com>
2019-06-13 13:51:36 +0800

    fix bugs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main(){
-    println!("hello world");
+mod rust_01_input;
+mod rust_02_guess;
+
+fn main() {
+    println!("start");
+
+    //接收键盘输入
+//    rust_01_input::run();
+
+    //猜猜看小游戏
+//    rust_02_guess::run();
+
+
+
 }
--- src/rust_02_guess.rs
+++ src/rust_02_guess.rs
@@ -1,16 +1,17 @@
 //需要先在Cargo.toml中添加随机数生成的依赖rand = "0.3.14"，因为Rust标准库中没有这个功能
 use rand::Rng;
 use std::io;
-
-fn main(){
+//猜猜看小游戏
+pub fn run() {
+    println!("hello world");
     println!("Guess the number");
-    println!("Please input your guess:");
     //生成随机数，1到100
     let random_num = rand::thread_rng().gen_range(1, 101);
     println!("the random number is {}", random_num);
+    println!("Please input your guess:");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess)
-            .expect("Filed to read line!");
+        .expect("Filed to read line!");
     println!("your guess {}", guess);
 }
 
